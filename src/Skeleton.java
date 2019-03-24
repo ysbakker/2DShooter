f9386e3ff8993527e581cb94d487201d98afd4d1
Yorrick Bakker <y.bakker@me.com>
2019-03-24 20:44:02 +0100

    Fixed small bug with amount of frames

--- src/Skeleton.java
+++ src/Skeleton.java
@@ -3,7 +3,7 @@ import nl.han.ica.oopg.objects.Sprite;
 public class Skeleton extends Enemy {
 
     public Skeleton(ShooterApp world) {
-        super(world, new Sprite("media/sprite_skeleton.png"), 8);
+        super(world, new Sprite("media/sprite_skeleton.png"), 9);
         System.out.println("skeleton spawned.");
 
         walkingSpeed = -2;
