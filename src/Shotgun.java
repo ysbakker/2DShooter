428af5cc365cdbddd6a1e3af35a1b3bedd44b614
Jogg_Jogg <jdobberr@gmail.com>
2019-03-29 13:17:58 +0100

    Shotgun.java changed particle sprite

--- src/Shotgun.java
+++ src/Shotgun.java
@@ -5,12 +5,12 @@ public class Shotgun extends Weapon {
         autoFire = false;
         fireDelay = 0.5;
         magSize = 0;
-        particlefn = "media/sprite_bullet.png";
+        particlefn = "media/sprite_shotgun_shell.png";
         damage = 20;
         particleSpeed = 10;
 
-        particleOffsetX = 20;
-        particleOffsetY = 25;
+        particleOffsetX = 47;
+        particleOffsetY = 33;
         weaponOffsetX = 20;
         weaponOffsetY = 30;
     }
